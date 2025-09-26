f89b1f0fe3a0f65701a98b784714717242446dab
SphereStacking <sphere.stacking@gmail.com>
2025-09-26 14:16:11 +0900

    ✨ SpeedInsightsコンポーネントの追加と依存関係の更新
    - app.vueにSpeedInsightsコンポーネントを追加
    - @vercel/speed-insightsを追加

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
+<script setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
 <template>
   <TooltipProvider>
+    <SpeedInsights/> 
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
